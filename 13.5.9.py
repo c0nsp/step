527e720944fcf2d0d7c2db3a0167011e80091dcb
c0nsp <d3n1@centrum.cz>
2022-08-21 16:00:31 +0200

    done

--- 13.5.9.py
+++ 13.5.9.py
@@ -1,1 +1,19 @@
-a = int(input())
+# объявление функции
+def convert_to_python_case(text):
+    s = txt[0].lower()
+    for i in range(1, len(txt)):
+        if txt[i].isupper():
+            s += '_' + txt[i].lower()
+        else:
+            s += txt[i]
+    return s
+    
+    
+    
+            
+
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(convert_to_python_case(txt))
