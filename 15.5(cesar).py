8ffd49e7395b636b0ba37f63fed754377599520c
c0nsp <d3n1@centrum.cz>
2022-08-25 17:37:35 +0200

    done

--- 15.5(cesar).py
+++ 15.5(cesar).py
@@ -1,12 +1,17 @@
-alph = ['АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
-direction = input('Выбери направление: \n(+) - Шифрование \n(-) - Дешифрование:\n ')
-lang = int(input('Выбери язык алфавита: \n0 - Русский \n1 - Английский: \n'))
-step = int(direction + input('Веди число, шаг сдвига (со сдвигом вправо): '))
-text = input('Введите текст для обработки:\n')
-
-for i in text:
-    if i.isalpha():
-        char = alph[lang][(alph[lang].index(i.upper()) + step) % len(alph[lang])]
-        print(char if i.isupper() else char.lower(), end='')
-    else:
-        print(i, end='')
+alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+s = input().split()
+x = ''
+for i in s:
+    step = 0
+    for j in i:
+        if j.isalpha() == True:
+            step += 1
+    for j in i:
+        text = alph.find(j)
+        new = text + step
+        if j.isalpha() == True:
+            x += alph[new]
+        else:
+            x += j
+    x += ' '
+print(x)
